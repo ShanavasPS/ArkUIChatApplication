4b62990a61be322e9c6a81f8e9284f6e61a92733
Shanavas Shaji <shanavas.shaji@huawei.com>
2023-10-12 16:42:00 +0300

    Added some dummy users

--- entry/src/main/ets/model/UserData.ts
+++ entry/src/main/ets/model/UserData.ts
@@ -4,11 +4,13 @@ let NextId = 0;
 export default class UserData {
   id: string;
   name: string;
+  email: string;
   image: string;
 
-  constructor(name: string, image: string) {
+  constructor(name: string, email: string, image: string) {
     this.id = `${ NextId++ }`;
     this.name = name;
+    this.email = email;
     this.image = image;
   }
 }
